38ac30595839366ab4d4a7410397cedadec3ae8a
tajbsta <tajbsta22@gmail.com>
2021-03-02 10:44:55 -0500

    Fix undefined socket that is related to screen resize  - [177150228] (#62)
    * fix undefined socket bug
    
    * use single state

--- src/components/GlobalsContextProvider/index.js
+++ src/components/GlobalsContextProvider/index.js
@@ -1,32 +1,37 @@
 import { h } from 'preact';
-import { useEffect } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 import io from 'socket.io-client';
 import { GlobalsContext } from 'Shared/context';
 
-let socket;
 const GlobalsContextProvider = ({ children }) => {
-  useEffect(() => {
-    const onConnect = () => {
-      console.log('socket connected');
-      // this needs to be exposed, we need to be able to choose which room we're connecting;
-      socket.emit('joinRoom', { room: '58347159', userId: 'zlUserId' });
-    };
+  const [socket, setSocket] = useState();
 
-    if (socket) {
-      console.warn('Socket already exists');
-      socket.close();
+  const onConnect = () => {
+    console.log('socket connected');
+    // this needs to be exposed, we need to be able to choose which room we're connecting;
+    socket.emit('joinRoom', { room: '58347159', userId: 'zlUserId' });
+  };
+
+  useEffect(() => {
+    if (!socket) {
+      setSocket(io(`${process.env.PREACT_APP_WS_PROTOCOL}${process.env.PREACT_APP_API_AUTHORITY}`));
     }
 
-    socket = io(`${process.env.PREACT_APP_WS_PROTOCOL}${process.env.PREACT_APP_API_AUTHORITY}`);
-    socket.on('connect', onConnect);
-
     return () => {
-      socket.off('connect', onConnect);
-      socket.disconnect();
-      socket = undefined;
-      console.log('socket disconnected');
+      if (socket) {
+        socket.off('connect', onConnect);
+        socket.disconnect();
+        setSocket(undefined);
+        console.log('socket disconnected');
+      }
     };
   }, []);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on('connect', onConnect);
+    }
+  }, [socket]);
 
   return (
     <GlobalsContext.Provider value={{ socket }}>
