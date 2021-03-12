e88ba6a68f0daec4b641f3a730a6f5407e4edea6
tajbsta <tajbsta22@gmail.com>
2021-03-12 09:47:38 -0500

    Placeholders to test Stripe Subscription State changes (#81)

--- src/routes/account/index.js
+++ src/routes/account/index.js
@@ -6,10 +6,14 @@ import {
   Tabs,
   Tab,
   Image,
+  Button,
 } from 'grommet';
 import { connect } from 'react-redux';
 import { useState } from 'preact/hooks';
 import { merge } from 'lodash-es';
+import useFetch from 'use-http';
+
+import { buildURL } from 'Shared/fetch';
 
 import grommetTheme from '../../grommetTheme';
 import EmailSection from './MyAccount/EmailSection';
@@ -34,6 +38,40 @@ const tabsTheme = {
 const Account = ({ profile }) => {
   const { animalIcon, color } = profile;
   const [activeIndex, setActiveIndex] = useState(2);
+  const { post } = useFetch(
+    buildURL(),
+    { credentials: 'include', cachePolicy: 'no-cache' },
+  );
+
+  // TODO: these methods, and the buttons uses them
+  // Are place holders that should be replaced once
+  // we work on #177150051
+  const cancelSubscription = async () => {
+    try {
+      await post('/users/subscription/unsubscribe');
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const retainUser = async () => {
+    // Define how we're going to get the discount from
+    try {
+      await post('/users/subscription/retain', { discount: 50 });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const updateSubscription = async () => {
+    // Update this once we implement the manage subscription
+    // Price ID should be sent based on what card the user clicked on
+    try {
+      await post('/users/subscription/6038074e28ce911198c70e0f/price_1INfVrGhk8ejMhuhRjfXkoH3');
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   return (
     <Box flex="grow">
@@ -74,7 +112,12 @@ const Account = ({ profile }) => {
             </Box>
           </Tab>
           <Tab title="Manage Subscription">
-            <Box pad="medium">Manage Subscription Tab</Box>
+            <Box pad="medium">
+              Manage Subscription Tab
+              <Button label="Cancel Subscription" onClick={cancelSubscription} />
+              <Button label="Retain User" onClick={retainUser} />
+              <Button label="Update Subscription" onClick={updateSubscription} />
+            </Box>
           </Tab>
         </Tabs>
       </Grommet>
