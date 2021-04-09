dd7e026224ff94467e05a8205e0d5edf9bf4a131
tajbsta <tajbsta22@gmail.com>
2021-04-09 13:57:30 +0000

    User menu on header (#126)

---
+++ src/components/Header/Menu/index.js
@@ -0,0 +1,97 @@
+import { h } from 'preact';
+import { route } from 'preact-router';
+import { useRef, useState } from 'preact/hooks';
+import { Drop, Box, Text } from 'grommet';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog, faQuestionCircle, faSignOut } from '@fortawesome/pro-solid-svg-icons';
+import useFetch from 'use-http';
+
+import { buildURL } from 'Shared/fetch';
+import AnimalIcon from 'Components/AnimalIcon';
+
+import style from './style.scss';
+
+const Menu = () => {
+  const buttonRef = useRef(null)
+  const [showMenu, setShowMenu] = useState(false);
+  const { post, response } = useFetch(
+    buildURL('/admin/users/logout'),
+    { credentials: 'include', cachePolicy: 'no-cache' },
+  );
+
+  const handleLogout = async () => {
+    await post();
+    if (response.ok) {
+      setShowMenu(false);
+      route('/login');
+    }
+  }
+
+  const accountHandler = () => {
+    setShowMenu(false);
+    route('/account')
+  }
+
+  return (
+    <div className={style.menuContainer}>
+      <Box
+        pad="12px"
+        align="center"
+        justify="start"
+        ref={buttonRef}
+        onClick={() => setShowMenu(!showMenu)}
+      >
+        <AnimalIcon />
+      </Box>
+      {showMenu && (
+        <Drop
+          align={{ top: 'bottom' }}
+          target={buttonRef.current}
+          elevation="xlarge"
+          className={style.menuItem}
+        >
+          <Box
+            width="195px"
+            height="35px"
+            pad={{ horizontal: '15px' }}
+            direction="row"
+            align="center"
+            onClick={accountHandler}
+          >
+            <FontAwesomeIcon icon={faCog} />
+            <Box margin={{ left: '12px' }}>
+              <Text size="large">My Account</Text>
+            </Box>
+          </Box>
+          <Box
+            width="195px"
+            height="35px"
+            pad={{ horizontal: '15px' }}
+            direction="row"
+            align="center"
+          >
+            <FontAwesomeIcon icon={faQuestionCircle} />
+            <Box margin={{ left: '12px' }}>
+              <Text size="large">Help</Text>
+            </Box>
+          </Box>
+          <Box
+            width="195px"
+            height="35px"
+            pad={{ horizontal: '15px' }}
+            direction="row"
+            align="center"
+            onClick={handleLogout}
+          >
+            <FontAwesomeIcon icon={faSignOut} />
+            <Box margin={{ left: '12px' }}>
+              <Text size="large">Log Out</Text>
+            </Box>
+          </Box>
+        </Drop>
+      )}
+    </div>
+  );
+};
+
+export default Menu;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,21 @@
 import { h } from 'preact';
-import { Link } from 'preact-router';
 import { Header } from 'grommet';
 
 import NavBar from '../NavBar';
 import ZoolifeLogo from '../ZoolifeLogo';
-import AnimalIcon from '../AnimalIcon';
-import Menu from '../async/Menu';
+import AdminMenu from '../async/Menu';
+import UserMenu from './Menu';
 
 import style from './style.scss';
 
 const HeaderComponent = () => (
   <Header className={style.header}>
-    <Menu />
+    <AdminMenu />
     <div className={style.logo}>
       <ZoolifeLogo />
     </div>
     <NavBar />
-    <div className={style.userImageSection}>
-      <Link href="/account">
-        <AnimalIcon />
-      </Link>
-    </div>
+    <UserMenu />
   </Header>
 );
 
