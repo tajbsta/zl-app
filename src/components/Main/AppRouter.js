e6b3871f90800cc408af8e5a3df9bfecc737a2de
Bojan Đurđević <bojan88@gmail.com>
2021-04-19 12:37:25 -0400

    Error pages header fix, and better logic [PT177817242] (#183)
    * Fix for error pages, and better header logic
    
    * Lint fix
    
    * Pass router data to habitat component

--- src/components/Main/AppRouter.js
+++ src/components/Main/AppRouter.js
@@ -1,5 +1,4 @@
 import { Router } from 'preact-router';
-import { connect } from 'react-redux';
 import { Box } from 'grommet';
 
 import AuthGuard from 'Components/Authorize/AuthGuard';
@@ -13,42 +12,63 @@ import Account from '../../routes/account';
 import Habitat from '../../routes/habitat';
 import Welcome from '../../routes/welcome';
 
-const AppRouter = ({ sessionChecked }) => (
-  <Box fill>
-    {sessionChecked && <Header />}
-
-    <Box fill pad={{ top: 'var(--headerHeight)' }}>
-      <Box fill overflow="auto">
-        <Box fill width={{ max: "1650px", min: "350px" }} margin={{ horizontal: 'auto' }}>
-          <Router>
-            <AuthGuard path="/welcome" title="Welcome to Zoolife" redirectTo="/" permission="welcome:view">
-              <Welcome />
-            </AuthGuard>
-            <AuthGuard path="/h/:zooName/:habitatSlug" permission="habitat:view" skipTitle redirectTo="/plans">
-              <Habitat />
-            </AuthGuard>
-            <AuthGuard path="/map" permission="map:view" title="Map" redirectTo="/plans">
-              <Map />
-            </AuthGuard>
-            <AuthGuard path="/schedule" permission="schedule:view" title="Talk Schedule" redirectTo="/plans">
-              <Schedule />
-            </AuthGuard>
-            <AuthGuard path="/favorite" permission="favorite:edit" title="Favorites" redirectTo="/plans">
-              <Favorite />
-            </AuthGuard>
-            <AuthGuard path="/account" permission="profile:edit">
-              <Account />
-            </AuthGuard>
-            <AuthGuard path="/plans" permission="checkout:plans" title="Plans">
-              <Plans />
-            </AuthGuard>
-          </Router>
-        </Box>
+const PageWrapper = ({ children }) => (
+  <Box fill pad={{ top: 'var(--headerHeight)' }}>
+    <Box fill overflow="auto">
+      <Box fill width={{ max: "1650px", min: "350px" }} margin={{ horizontal: 'auto' }}>
+        {children}
       </Box>
     </Box>
   </Box>
 );
 
-export default connect(
-  ({ user: { sessionChecked } }) => ({ sessionChecked }),
-)(AppRouter);
+const AppRouter = ({ matches }) => (
+  <Box fill>
+    <Router>
+      <AuthGuard path="/welcome" title="Welcome to Zoolife" redirectTo="/" permission="welcome:view">
+        <Header />
+        <PageWrapper>
+          <Welcome />
+        </PageWrapper>
+      </AuthGuard>
+      <AuthGuard path="/h/:zooName/:habitatSlug" permission="habitat:view" skipTitle redirectTo="/plans">
+        <Header />
+        <PageWrapper>
+          <Habitat matches={matches} />
+        </PageWrapper>
+      </AuthGuard>
+      <AuthGuard path="/map" permission="map:view" title="Map" redirectTo="/plans">
+        <Header />
+        <PageWrapper>
+          <Map />
+        </PageWrapper>
+      </AuthGuard>
+      <AuthGuard path="/schedule" permission="schedule:view" title="Talk Schedule" redirectTo="/plans">
+        <Header />
+        <PageWrapper>
+          <Schedule />
+        </PageWrapper>
+      </AuthGuard>
+      <AuthGuard path="/favorite" permission="favorite:edit" title="Favorites" redirectTo="/plans">
+        <Header />
+        <PageWrapper>
+          <Favorite />
+        </PageWrapper>
+      </AuthGuard>
+      <AuthGuard path="/account" permission="profile:edit">
+        <Header />
+        <PageWrapper>
+          <Account />
+        </PageWrapper>
+      </AuthGuard>
+      <AuthGuard path="/plans" permission="checkout:plans" title="Plans">
+        <Header />
+        <PageWrapper>
+          <Plans />
+        </PageWrapper>
+      </AuthGuard>
+    </Router>
+  </Box>
+);
+
+export default AppRouter;
