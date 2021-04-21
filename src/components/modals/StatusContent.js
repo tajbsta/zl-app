27c0cd208a2643eda6724cf454a3794b7631d42c
tajbsta <tajbsta22@gmail.com>
2021-04-21 19:52:51 +0000

    Adding contact us button to the status content component (#210)

--- src/components/modals/StatusContent.js
+++ src/components/modals/StatusContent.js
@@ -1,14 +1,24 @@
 import { h } from 'preact';
 import {
   Box,
-  Button,
   Heading,
   Text,
+  Button,
 } from 'grommet';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle, faTimes, faTimesCircle } from '@fortawesome/pro-regular-svg-icons';
+import { connect } from 'react-redux';
 
-const StatusContent = ({ type = 'success', text, onClose }) => (
+import { PrimaryButton, OutlineButton } from 'Components/Buttons';
+
+import { openContactUsModal } from 'Components/modals/ContactUs/actions';
+
+const StatusContent = ({
+  type = 'success',
+  text,
+  onClose,
+  openContactUsModalAction,
+}) => (
   <Box width="400px">
     <Box
       direction="row"
@@ -54,11 +64,16 @@ const StatusContent = ({ type = 'success', text, onClose }) => (
 
       <Text textAlign="center" size="large">{text}</Text>
 
-      <Box width={{ min: '140px' }} margin={{ vertical: 'large' }}>
-        <Button primary size="large" label="Okay" onClick={onClose} />
+      <Box width={{ min: '140px' }} margin={{ vertical: '40px' }}>
+        <PrimaryButton size="large" label="Okay" onClick={onClose} />
+        {type === 'error' && (
+          <Box margin={{ top: '30px' }}>
+            <OutlineButton size="large" label="Contact Us" onClick={openContactUsModalAction} />
+          </Box>
+        )}
       </Box>
     </Box>
   </Box>
 );
 
-export default StatusContent;
+export default connect(null, { openContactUsModalAction: openContactUsModal })(StatusContent);
