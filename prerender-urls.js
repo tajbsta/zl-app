6dba1ae56d908745e474f804bc4e0060eb87ae19
tajbsta <tajbsta22@gmail.com>
2021-04-22 20:09:44 +0000

    Adding seo tags (title, description, and og:image) (#225)

--- prerender-urls.js
+++ prerender-urls.js
@@ -4,11 +4,11 @@
 require('dotenv').config()
 
 module.exports = [
-  { url: '/', title: 'Zoolife', segmentId: process.env.PREACT_APP_SEGMENT_ID },
-  { url: '/oranapark', title: 'Zoolife - Orana Zoo', segmentId: process.env.PREACT_APP_SEGMENT_ID },
-  { url: '/torontozoo', title: 'Zoolife - Toronto Zoo', segmentId: process.env.PREACT_APP_SEGMENT_ID },
-  { url: '/pmmc', title: 'Zoolife - Pacific Marine Mammal Center', segmentId: process.env.PREACT_APP_SEGMENT_ID },
-  { url: '/sazoo', title: 'Zoolife - San Antonio Zoo', segmentId: process.env.PREACT_APP_SEGMENT_ID },
+  { url: '/', title: 'Zoolife - The world\'s first digital zoo.', segmentId: process.env.PREACT_APP_SEGMENT_ID },
+  { url: '/oranapark', title: 'Zoolife - The world\'s first digital zoo. - Orana Park', segmentId: process.env.PREACT_APP_SEGMENT_ID },
+  { url: '/torontozoo', title: 'Zoolife - The world\'s first digital zoo. - Toronto Zoo', segmentId: process.env.PREACT_APP_SEGMENT_ID },
+  { url: '/pmmc', title: 'Zoolife - The world\'s first digital zoo. - Pacific Marine Mammal Center', segmentId: process.env.PREACT_APP_SEGMENT_ID },
+  { url: '/sazoo', title: 'Zoolife - The world\'s first digital zoo. - San Antonio Zoo', segmentId: process.env.PREACT_APP_SEGMENT_ID },
 
   // this will prerender only loading indicator
   // we'll use this as fallback on s3
