1f412a841dbe7351e023af03da5256dcbe2a1932
tajbsta <tajbsta22@gmail.com>
2021-04-22 14:52:46 +0000

    Including segment id on prerender data (#216)

--- prerender-urls.js
+++ prerender-urls.js
@@ -1,11 +1,16 @@
+// This is loaded before envVars on preact.config
+// hence dotenv will be used to inject .env
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config()
+
 module.exports = [
-  { url: '/', title: 'Zoolife' },
-  { url: '/oranapark', title: 'Zoolife - Orana Zoo' },
-  { url: '/torontozoo', title: 'Zoolife - Toronto Zoo' },
-  { url: '/pmmc', title: 'Zoolife - Pacific Marine Mammal Center' },
-  { url: '/sazoo', title: 'Zoolife - San Antonio Zoo' },
+  { url: '/', title: 'Zoolife', segmentId: process.env.PREACT_APP_SEGMENT_ID },
+  { url: '/oranapark', title: 'Zoolife - Orana Zoo', segmentId: process.env.PREACT_APP_SEGMENT_ID },
+  { url: '/torontozoo', title: 'Zoolife - Toronto Zoo', segmentId: process.env.PREACT_APP_SEGMENT_ID },
+  { url: '/pmmc', title: 'Zoolife - Pacific Marine Mammal Center', segmentId: process.env.PREACT_APP_SEGMENT_ID },
+  { url: '/sazoo', title: 'Zoolife - San Antonio Zoo', segmentId: process.env.PREACT_APP_SEGMENT_ID },
 
   // this will prerender only loading indicator
   // we'll use this as fallback on s3
-  { url: '/account' },
+  { url: '/account', segmentId: process.env.PREACT_APP_SEGMENT_ID },
 ];
