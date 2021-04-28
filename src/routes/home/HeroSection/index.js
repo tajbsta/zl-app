feba0492126c5c7451020cc8671ff8c9b696ca78
tajbsta <tajbsta22@gmail.com>
2021-04-28 17:12:00 -0400

    move text change to js because content change in pseudo elements is not supported on safari, disable animation until there is a solution to the make js and css in sync without a flickering (#242)

--- src/routes/home/HeroSection/index.js
+++ src/routes/home/HeroSection/index.js
@@ -1,51 +1,71 @@
 import { h } from 'preact';
 import { route } from 'preact-router';
 import { LandingSecondary } from 'Components/Buttons';
+import { useState, useEffect } from 'preact/hooks';
 
 import zoolifeLogoWhite from './zoolife-white.png';
 
 import style from './style.scss';
 
-const HeroSection = ({ partnerImage }) => (
-  <div className={style.heroContainer}>
-    <div className={style.top}>
-      <div className={style.left}>
-        <div>
-          <div>
-            {partnerImage && (
-              <div className={style.partner}>
-                <img alt="zoolife-logo" src={zoolifeLogoWhite} />
-                <span>x</span>
-                <img alt="partner-logo" src={partnerImage} />
-              </div>
-            )}
+const rotatingText = ['Explore', 'Protect', 'Observe', 'Discover', 'Support'];
 
-            <h1>
-              <div className={style.animationContainer}>
-                <span />
-                &nbsp;
-              </div>
-              nature from home.
-            </h1>
-            <h4>
-              Live animal experiences from the world&apos;s top zoos, hosted by nature experts.
-            </h4>
-            <LandingSecondary onClick={() => route('/signup')}>Meet the Animals</LandingSecondary>
+const HeroSection = ({ partnerImage }) => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const timeout = setTimeout(
+      () => {
+        setIndex(index === rotatingText.length - 1 ? 0 : index + 1 );
+      },
+      2500,
+    );
+
+    return () => clearTimeout(timeout);
+  }, [index]);
+
+  return (
+    <div className={style.heroContainer}>
+      <div className={style.top}>
+        <div className={style.left}>
+          <div>
+            <div>
+              {partnerImage && (
+                <div className={style.partner}>
+                  <img alt="zoolife-logo" src={zoolifeLogoWhite} />
+                  <span>x</span>
+                  <img alt="partner-logo" src={partnerImage} />
+                </div>
+              )}
+
+              <h1>
+                <div className={style.animationContainer}>
+                  <span className={style.animatedText}>
+                    {rotatingText[index]}
+                  </span>
+                  &nbsp;
+                </div>
+                nature from home.
+              </h1>
+              <h4>
+                Live animal experiences from the world&apos;s top zoos, hosted by nature experts.
+              </h4>
+              <LandingSecondary onClick={() => route('/signup')}>Meet the Animals</LandingSecondary>
+            </div>
           </div>
         </div>
+        <div className={style.right}>
+          <video muted autoPlay loop playsInline controls={false}>
+            <source src="https://assets.zoolife.tv/landing/s1.webm" type="video/webm" />
+            <source src="https://assets.zoolife.tv/landing/s1.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
       </div>
-      <div className={style.right}>
-        <video muted autoPlay loop playsInline controls={false}>
-          <source src="https://assets.zoolife.tv/landing/s1.webm" type="video/webm" />
-          <source src="https://assets.zoolife.tv/landing/s1.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+      <div className={style.bottom}>
+        <h4>50% of your purchase funds animal care &amp; conservation.</h4>
       </div>
     </div>
-    <div className={style.bottom}>
-      <h4>50% of your purchase funds animal care &amp; conservation.</h4>
-    </div>
-  </div>
-);
+  );
+};
 
 export default HeroSection;
