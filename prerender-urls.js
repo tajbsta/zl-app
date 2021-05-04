14dd861fbea2f14e0e327ce275424d368d5632d1
tajbsta <tajbsta22@gmail.com>
2021-05-04 16:30:42 +0000

    Renaming Orana Park title (#251)

--- prerender-urls.js
+++ prerender-urls.js
@@ -5,8 +5,8 @@ require('dotenv').config()
 
 module.exports = [
   { url: '/', title: 'Zoolife - The world\'s first digital zoo.', segmentId: process.env.PREACT_APP_SEGMENT_ID },
-  { url: '/oranapark', title: 'Zoolife - The world\'s first digital zoo. - Orana Park', segmentId: process.env.PREACT_APP_SEGMENT_ID },
-  { url: '/orana', title: 'Zoolife - The world\'s first digital zoo. - Orana Park', segmentId: process.env.PREACT_APP_SEGMENT_ID },
+  { url: '/oranapark', title: 'Zoolife - The world\'s first digital zoo. - Orana Wildlife Park', segmentId: process.env.PREACT_APP_SEGMENT_ID },
+  { url: '/orana', title: 'Zoolife - The world\'s first digital zoo. - Orana Wildlife Park', segmentId: process.env.PREACT_APP_SEGMENT_ID },
   { url: '/torontozoo', title: 'Zoolife - The world\'s first digital zoo. - Toronto Zoo', segmentId: process.env.PREACT_APP_SEGMENT_ID },
   { url: '/pmmc', title: 'Zoolife - The world\'s first digital zoo. - Pacific Marine Mammal Center', segmentId: process.env.PREACT_APP_SEGMENT_ID },
   { url: '/sazoo', title: 'Zoolife - The world\'s first digital zoo. - San Antonio Zoo', segmentId: process.env.PREACT_APP_SEGMENT_ID },
