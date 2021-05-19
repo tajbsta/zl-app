6badb62e4c6a5a4215818bb4254d42edd5c612c2
tajbsta <tajbsta22@gmail.com>
2021-05-19 12:36:29 -0400

    add line break and remove comma for family members (#317)

--- src/routes/habitat/components/StreamProfile/members/member/index.js
+++ src/routes/habitat/components/StreamProfile/members/member/index.js
@@ -24,7 +24,12 @@ const Member = ({
 
     <Text as="p" size="medium" margin="0 8px" className={style.text}>
       {name}
-      {age ? `, ${age}` : ''}
+      {age && (
+        <>
+          <br />
+          {age}
+        </>
+      )}
     </Text>
   </div>
 );
