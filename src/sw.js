b58e967a5fdd278555883145e57dfadbc8b4677f
Bojan Đurđević <bojan88@gmail.com>
2021-06-25 13:57:31 -0400

    Added onboarding video to precaching files [PT178527643] (#448)

--- src/sw.js
+++ src/sw.js
@@ -3,7 +3,10 @@ import { registerRoute } from 'workbox-routing';
 import { CacheFirst } from 'workbox-strategies';
 
 setupRouting();
-setupPrecaching(getFiles());
+setupPrecaching([
+  ...getFiles(),
+  { url: 'https://zoolife.tv/assets/zoolife_onboarding.mp4', revision: null },
+]);
 
 registerRoute(
   'https://zoolife.tv/assets/zoolife_onboarding.mp4',
