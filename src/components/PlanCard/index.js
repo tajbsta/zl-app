c2c7124b8d181e86de533592d1dcb89ce0288dc6
tajbsta <tajbsta22@gmail.com>
2021-06-28 16:45:51 -0400

    Changing discount text to Aug 1st (#458)

--- src/components/PlanCard/index.js
+++ src/components/PlanCard/index.js
@@ -79,7 +79,7 @@ const PlanCard = ({
             </Box>
             <Box align="center" margin={{ top: '10px'}}>
               <Text size="10px" style={{ lineHeight: '11.5px'}}>
-                Discount Pricing Ends Jun 30th
+                Discount Pricing Ends Aug 1st
               </Text>
             </Box>
           </Box>
