efa1feea7c4e117d09acb73b347cb2df6e7014fd
tajbsta <tajbsta22@gmail.com>
2021-07-09 16:22:53 -0400

    [178773654] - Its possible to react to messages on chat (#486)
    * wip
    
    * Adding reactions to messages
    
    * lint fix
    
    * css fixes
    
    * better positioning for emotes
    
    * layout changes as request by the design team
    
    * lint fix
    
    * css fixes

---
+++ src/components/Chat/ChatContainer/ChatMessage/ReactionBadge/index.js
@@ -0,0 +1,23 @@
+import { Text } from 'grommet';
+import classnames from 'classnames';
+
+import images from '../ReactionBar/assets';
+import style from './style.module.scss';
+
+const ReactionBadge = ({
+  reaction,
+  count,
+  isReaction,
+  onClick,
+}) => (
+  // eslint-disable-next-line
+  <div
+    className={classnames(style.reactionContainer, {[style.reacted]: isReaction })}
+    onClick={() => onClick(reaction)}
+  >
+    <img src={images[reaction]} alt={`react with ${reaction}`} />
+    <Text size="10px">{count}</Text>
+  </div>
+);
+
+export default ReactionBadge;
--- src/components/Chat/ChatContainer/ChatMessage/index.js
+++ src/components/Chat/ChatContainer/ChatMessage/index.js
@@ -4,13 +4,23 @@ import {
   useEffect,
   useRef,
   useMemo,
+  useCallback,
 } from 'preact/hooks';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/pro-solid-svg-icons';
+import { faSmilePlus, faTrash } from '@fortawesome/pro-regular-svg-icons';
+import { Drop, Box } from 'grommet';
+import { usePubNub } from 'pubnub-react';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
 
 import { getIconUrl } from 'Shared/profileIcons';
 import Can from 'Components/Authorize';
+import ReactionBar from './ReactionBar';
+import ReactionBadge from './ReactionBadge';
+
 import AnimalIcon from '../../../AnimalIcon';
+
+import { useIsHabitatTabbed } from '../../../../hooks';
 
 import style from './style.module.scss';
 
@@ -21,14 +31,65 @@ const ChatMessage = ({
   text,
   timestamp,
   timetoken,
+  reactions,
+  userId,
+  habitatId,
   onDeleteHandler,
 }) => {
+  const pubnub = usePubNub();
+  const [showActionBar, setShowActionbar] = useState(false);
+  const [showReactionBar, setShowReactionBar] = useState(false);
+  const isTabbedView = useIsHabitatTabbed();
+
+  const userReactions = useMemo(() => {
+    const userReactions = {};
+
+    Object.keys(reactions).forEach((reaction) => {
+      const userR = reactions[reaction].filter(
+        ({ uuid }) => (uuid === userId),
+      ).shift();
+
+      if (userR) {
+        userReactions[reaction] = userR.actionTimetoken;
+      }
+    });
+
+    return userReactions;
+  }, [reactions, userId]);
+
+  const handleReactionBarClose = useCallback(() => {
+    setShowReactionBar(false);
+    setShowActionbar(false);
+  }, [])
+  const toggleReaction = useCallback((reaction) => {
+    if (!userReactions[reaction]) {
+      pubnub.addMessageAction({
+        channel: habitatId,
+        messageTimetoken: timetoken,
+        action: {
+          type: 'reaction',
+          value: reaction,
+        },
+      });
+    } else {
+      pubnub.removeMessageAction(
+        {
+          channel: habitatId,
+          messageTimetoken: timetoken,
+          actionTimetoken: userReactions[reaction],
+        },
+      );
+    }
+    setShowReactionBar(false);
+  }, [pubnub, timetoken, userReactions, habitatId]);
+
   const initialMessage = useMemo(() => {
     const message = formatDistanceToNowStrict(timestamp, { roundingMethod: 'ceil'});
     return message.startsWith('0') ? '1 minute' : message;
   }, [timestamp]);
   const [messageTime, setMessageTime] = useState(initialMessage);
   const intervalRef = useRef(null);
+  const messageContainer = useRef();
 
   useEffect(() => {
     intervalRef.current = setInterval(() => {
@@ -40,36 +101,87 @@ const ChatMessage = ({
   }, [messageTime, timestamp]);
 
   return (
-    <div className={style.chatMessageContainer}>
-      <AnimalIcon
-        animalIcon={animal.endsWith('.svg') ? animal : getIconUrl(animal)}
-        color={color}
-        width={26}
-      />
-      <div className={style.messageWrapper}>
-        <div className={style.headerWrapper}>
-          <span className={style.userName}>
-            {username}
+    <>
+      <div
+        className={classnames(
+          style.chatMessageContainer,
+          {[style.selected]: showReactionBar || showActionBar },
+        )}
+        ref={messageContainer}
+        onMouseEnter={() => setShowActionbar(true)}
+        onMouseLeave={() => setShowActionbar(showReactionBar)}
+      >
+        <AnimalIcon
+          animalIcon={animal.endsWith('.svg') ? animal : getIconUrl(animal)}
+          color={color}
+          width={26}
+        />
+        <div className={style.messageWrapper}>
+          <div className={style.headerWrapper}>
+            <span className={style.userName}>
+              {username}
+            </span>
+            <span className={style.messageTime}>
+              {` ${messageTime} ago`}
+            </span>
+
+            <div className={classnames(style.actionBar, { [style.hide]: !showActionBar})}>
+              {/* eslint-disable-next-line */}
+              <div
+                className={style.actionButton}
+                onClick={() => setShowReactionBar(true)}
+              >
+                <FontAwesomeIcon icon={faSmilePlus} />
+              </div>
+              <Can
+                  perform="chat:moderate"
+                  yes={() => (
+                    // eslint-disable-next-line
+                    <div className={style.actionButton} onClick={() => onDeleteHandler(timetoken)}>
+                      <FontAwesomeIcon icon={faTrash} />
+                    </div>
+                  )}
+              />
+            </div>
+          </div>
+          <span className={style.message}>
+            {text}
           </span>
-          <span className={style.messageTime}>
-            {` ${messageTime} ago`}
-          </span>
-          <Can
-              perform="chat:moderate"
-              yes={() => (
-                // eslint-disable-next-line
-                <span className={style.delete} onClick={() => onDeleteHandler(timetoken)}>
-                  <FontAwesomeIcon icon={faTimes} />
-                </span>
-              )}
-          />
+          <Box direction="row" gap="5px" wrap>
+            {reactions && Object.keys(reactions).map((reaction) => (
+              <ReactionBadge
+                reaction={reaction}
+                count={reactions[reaction].length}
+                isReaction={userReactions[reaction] ?? false}
+                onClick={toggleReaction}
+              />
+            ))}
+          </Box>
         </div>
-        <span className={style.message}>
-          {text}
-        </span>
       </div>
-    </div>
+      {messageContainer.current && showReactionBar && (
+        <Drop
+          align={ !isTabbedView ? { top: 'top', left: 'right' } : { top: 'top', right: 'left' }}
+          target={messageContainer.current}
+          width={{ min: isTabbedView ? '95%' : '90px', max: '350px' }}
+          onClickOutside={handleReactionBarClose}
+          onEsc={handleReactionBarClose}
+          margin={{ horizontal: isTabbedView ? '10px' : '-5px'}}
+          stretch={false}
+        >
+          <ReactionBar onClick={toggleReaction} />
+        </Drop>
+      )}
+    </>
   );
 }
 
-export default ChatMessage;
+export default connect(({
+  user: {
+    userId,
+  },
+  habitat: { habitatInfo: { _id: habitatId } },
+}) => ({
+  userId,
+  habitatId,
+}))(ChatMessage);
