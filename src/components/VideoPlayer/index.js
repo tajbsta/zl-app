dc97662b0ad48fb43f1582ebd2f97d0b31f702fa
tajbsta <tajbsta22@gmail.com>
2021-08-10 08:58:11 -0400

    [179163501] - Add next button to the offline content container (#567)
    * Adding next button to the offline container
    
    * enhancements on the controls hide/show logic

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -10,6 +10,8 @@ import { buildURL } from 'Shared/fetch';
 import Loader from 'Components/Loader';
 import VideoControls from 'Components/VideoControls';
 
+import { useIsHabitatTabbed } from '../../hooks';
+
 const VideoPlayer = ({
   videoURL,
   videoId,
@@ -18,24 +20,51 @@ const VideoPlayer = ({
   onEnd,
   onLoad,
   className,
-  autoPlay,
+  autoPlay = false,
   muted = false,
   logView = true,
   isGuest,
+  onNextHandler,
 }) => {
   const videoRef = useRef();
+  const timeoutRef = useRef();
   const [hasUserPlayed, setHasUserPlayed] = useState(false);
-  const [isPlaying, setIsPlaying] = useState(autoPlay);
+  const [isPlaying, setIsPlaying] = useState(autoPlay );
   const [isLoading, setIsLoading] = useState(true);
   const [videoLength, setVideoLength] = useState(0);
   const [timeElapsed, setTimeElapsed] = useState(0);
+  const [showControls, setShowControls] = useState(false);
+  const isMobile = useIsHabitatTabbed();
+
+  useEffect(() => {
+    if (!isMobile) {
+      setShowControls(true);
+    }
+  }, [isMobile]);
+
+  const handleScreenClick = useCallback(() => {
+    if (isMobile) {
+      const action = showControls ? 'show' : 'hide';
+      setShowControls(!showControls);
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+
+      if (action === 'show') {
+        timeoutRef.current = setTimeout(() => {
+          setShowControls(false);
+        }, 5000);
+      }
+    }
+  }, [isMobile, showControls]);
 
   const { put: setVideoAsViewed } = useFetch(
     buildURL(`/videos/${videoId}/viewed`),
     { credentials: 'include', cachePolicy: 'no-cache' },
   );
 
-  const onPlay = () => {
+  const onPlay = useCallback(() => {
+    handleScreenClick();
     if (!hasUserPlayed && logView) {
       setHasUserPlayed(true);
       if (!isGuest) {
@@ -43,9 +72,10 @@ const VideoPlayer = ({
       }
     }
     setIsPlaying(true);
-  }
+  }, [handleScreenClick, hasUserPlayed, isGuest, logView, setVideoAsViewed]);
 
   const onPause = () => {
+    handleScreenClick();
     if (isPlaying) {
       videoRef.current.pause();
       setIsPlaying(false);
@@ -66,19 +96,19 @@ const VideoPlayer = ({
     }
   }, [onEnd]);
 
-  const onLoadMetadata = () => {
+  const onLoadMetadata = useCallback(() => {
     setVideoLength(Math.round(videoRef.current.duration));
     if (autoPlay) {
       videoRef.current.play();
     }
-  };
+  }, [autoPlay]);
 
-  const onLoadedData = () => {
+  const onLoadedData = useCallback(() => {
     if (typeof onLoad === 'function') {
       onLoad();
     }
     setIsLoading(false)
-  };
+  }, [onLoad])
 
   const updateVideoTimeHandler = useCallback((value) => {
     videoRef.current.currentTime = value;
@@ -92,13 +122,22 @@ const VideoPlayer = ({
     setTimeElapsed(0);
   }, [videoURL]);
 
+  const onSuspendeHandler = useCallback(() => {
+    if (isMobile && !isPlaying) {
+      setIsLoading(false);
+      setIsPlaying(false);
+      handleScreenClick();
+    }
+  }, [handleScreenClick, isMobile, isPlaying]);
+
   useEffect(() => {
     if (videoRef?.current) {
       videoRef.current.addEventListener('ended', onEndHandler);
       videoRef.current.addEventListener('play', onPlay);
       videoRef.current.addEventListener('timeupdate', onTimeUpdate);
       videoRef.current.addEventListener('loadedmetadata', onLoadMetadata);
-      videoRef.current.addEventListener('loadeddata', onLoadedData)
+      videoRef.current.addEventListener('loadeddata', onLoadedData);
+      videoRef.current.addEventListener('suspend', onSuspendeHandler);
     }
 
     return () => {
@@ -108,16 +147,30 @@ const VideoPlayer = ({
         videoRef.current.removeEventListener('timeupdate', onTimeUpdate);
         videoRef.current.removeEventListener('loadedmetadata', onLoadMetadata);
         videoRef.current.removeEventListener('loadeddata', onLoadedData)
+        videoRef.current.removeEventListener('suspend', onSuspendeHandler);
       }
     }
-  }, [videoRef.current, videoURL]);
+  }, [
+    videoRef,
+    videoURL,
+    onEndHandler,
+    onPlay,
+    onTimeUpdate,
+    onLoadMetadata,
+    onLoadedData,
+    onSuspendeHandler,
+  ]);
 
   return (
-    <>
+    <div
+      onMouseEnter={() => setShowControls(true)}
+      onMouseLeave={() => setShowControls(false)}
+      onClick={handleScreenClick}
+    >
       {isLoading && <Loader fill absolute />}
       {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
       <video
-        src={videoURL}
+        src={`${videoURL}#t=0.1`}
         playsInline
         style={{ width, height }}
         ref={videoRef}
@@ -126,7 +179,7 @@ const VideoPlayer = ({
       />
       <VideoControls
         ref={videoRef}
-        showControls
+        showControls={showControls}
         showPlayControl
         showVolumeControl
         showFullscreenControl
@@ -140,8 +193,9 @@ const VideoPlayer = ({
         videoLength={videoLength}
         timeElapsed={timeElapsed}
         updateVideoTimeHandler={updateVideoTimeHandler}
+        onNextHandler={onNextHandler}
       />
-    </>
+    </div>
   );
 };
 
