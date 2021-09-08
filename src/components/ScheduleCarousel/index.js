5681a332e4b42f0be35ff686370399df0d4ea75d
tajbsta <tajbsta22@gmail.com>
2021-09-08 10:13:54 -0400

    [179376338] - Update the weather widget to include the calendar (#620)

--- src/components/ScheduleCarousel/index.js
+++ src/components/ScheduleCarousel/index.js
@@ -1,30 +1,22 @@
 import { h } from 'preact';
 import {
-  useEffect,
   useMemo,
   useRef,
-  useState,
 } from "preact/hooks";
 import { connect } from 'react-redux';
 import Carousel from 'react-multi-carousel';
-import { format } from 'date-fns';
 import { isEmpty } from 'lodash-es';
 import classnames from 'classnames';
 
-import Card from '../Card';
 import PreviewVideo from '../Card/PreviewVideo';
-import { useUpcomingTalks } from '../../routes/habitat/hooks';
 
 import 'react-multi-carousel/lib/styles.css';
 import style from './style.scss';
 
 const ScheduleCarousel = ({
-  habitatId,
   previewVideo,
 }) => {
   const ref = useRef();
-  const [initiallyLoaded, setInitiallyLoaded] = useState(false);
-  const { loading, error, upcoming = [] } = useUpcomingTalks(habitatId, 1);
   const list = useMemo(() => {
     const items = [];
 
@@ -33,78 +25,38 @@ const ScheduleCarousel = ({
     }
     return items;
   }, [previewVideo]);
-  const liveTalks = useMemo(
-    () => upcoming.map(({ startTime, isStreamLive, ...rest }) => ({
-      text: `TALK | ${format(startTime, 'EEE hh:mm aa').toUpperCase()}`,
-      startTime,
-      ...rest,
-    })),
-    [upcoming],
-  );
 
-  useEffect(() => {
-    if (!loading) {
-      setInitiallyLoaded(true);
-    }
-  }, [loading]);
+  if (!list.length) {
+    return null;
+  }
 
   return (
     <div className={style.container}>
-      {!initiallyLoaded && <Card loading />}
-      {/* TODO: add error UI */}
-
-      {initiallyLoaded && !error && (
-        <Carousel
-          ref={ref}
-          dotListClass={style.dots}
-          slidesToSlide={1}
-          swipeable
-          additionalTransfrom={-30}
-          arrows={false}
-          autoPlaySpeed={3000}
-          draggable
-          minimumTouchDrag={80}
-          partialVisible
-          renderDotsOutside
-          showDots
-          keyBoardControl={false}
-          className={classnames(style.carousel, {
-            [style.singleItem]: (liveTalks.length + list.length) === 1,
-          }) }
-          responsive={{
-            generic: {
-              breakpoint: { max: 3000, min: 0 },
-              items: 1,
-              partialVisibilityGutter: 20,
-            },
-          }}
-        >
-          {list}
-
-          {liveTalks.map(({
-            _id,
-            title,
-            startTime,
-            zoo,
-            text,
-            profileImage,
-            description,
-            habitatId,
-          }) => (
-            <Card
-              key={_id}
-              scheduleId={_id}
-              title={title}
-              zoo={zoo}
-              startTime={startTime}
-              header={text}
-              description={description}
-              image={profileImage}
-              habitatId={habitatId}
-            />
-          ))}
-        </Carousel>
-      )}
+      <Carousel
+        ref={ref}
+        dotListClass={style.dots}
+        slidesToSlide={1}
+        swipeable
+        additionalTransfrom={-30}
+        arrows={false}
+        autoPlaySpeed={3000}
+        draggable
+        minimumTouchDrag={80}
+        partialVisible
+        renderDotsOutside
+        showDots
+        keyBoardControl={false}
+        className={classnames(style.carousel, style.singleItem)}
+        responsive={{
+          generic: {
+            breakpoint: { max: 3000, min: 0 },
+            items: 1,
+            partialVisibilityGutter: 20,
+          },
+        }}
+      >
+        {list}
+      </Carousel>
     </div>
   );
 };
--- src/components/WeatherWidget/index.js
+++ src/components/WeatherWidget/index.js
@@ -1,14 +1,44 @@
 import { connect } from 'react-redux';
-import { Box, Image, Text } from 'grommet';
+import {
+  Box,
+  Image,
+  Text,
+  Tip,
+} from 'grommet';
 import classnames from 'classnames';
+import { useMemo } from 'preact/hooks';
+import { format } from 'date-fns';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarAlt } from '@fortawesome/pro-light-svg-icons';
 
 import Clock from './Clock';
 import { useIsMobileSize } from '../../hooks';
+import { useUpcomingTalks } from '../../routes/habitat/hooks';
+import { showScheduleModal } from '../../routes/habitat/components/ScheduleModal/actions';
 
 import style from './style.scss';
 
-const WeatherWidget = ({ weather, location }) => {
+const WeatherWidget = ({
+  weather,
+  location,
+  habitatId,
+  showScheduleModalAction,
+}) => {
   const isMobileSize = useIsMobileSize();
+  const {upcoming = [] } = useUpcomingTalks(habitatId, 1);
+
+  const nextTalk = useMemo(
+    () => {
+      if (!upcoming?.length) {
+        return null;
+      }
+      const [nextTalk] = upcoming;
+      return format(nextTalk.startTime, 'EEE hh:mm aa').toUpperCase();
+    },
+    [upcoming],
+  );
+
+  console.log({ nextTalk });
 
   if (!weather || !location) {
     return null;
@@ -28,29 +58,62 @@ const WeatherWidget = ({ weather, location }) => {
       responsive
       width="small"
     >
-      <Box direction="row" flex="grow" margin={{ bottom: "xsmall" }}>
-        <Box className={style.liveTag}>
-          LIVE
-        </Box>
-        <Text size="medium">
-          {`${location.city}, ${location.country}`}
-        </Text>
-      </Box>
-      <Box direction="row" justify="center" align="center">
-        <Clock />
-        <Box flex="grow" justify="center" className={style.weather} direction="row">
-          <Image src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="condition" />
-          <Text size="small" weight={700}>
-            {`${parseInt(weather.temperature, 10)}C`}
+      <Box className={style.weatherWrapper}>
+        <Box direction="row" flex="grow" margin={{ bottom: "xsmall" }}>
+          <Box className={style.liveTag}>
+            LIVE
+          </Box>
+          <Text size="medium">
+            {`${location.city}, ${location.country}`}
           </Text>
         </Box>
+        <Box direction="row" justify="center" align="center">
+          <Clock />
+          <Box flex="grow" justify="center" className={style.weather} direction="row">
+            <Image src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="condition" />
+            <Text size="small" weight={700}>
+              {`${parseInt(weather.temperature, 10)}C`}
+            </Text>
+          </Box>
+        </Box>
       </Box>
+      <Tip
+        content={
+          <Box background="white" pad="xsmall" width="150px">
+            <Text size="small" textAlign="center">Click to see the talk schedule</Text>
+          </Box>
+        }
+      >
+        <Box
+          className={style.schedule}
+          direction="row"
+          flex="grow"
+          onClick={showScheduleModalAction}
+        >
+          {nextTalk && (
+            <>
+              <FontAwesomeIcon icon={faCalendarAlt} color="white" size={10} />
+              <Text size="small">
+                {`Next talk: ${nextTalk}`}
+              </Text>
+            </>
+          )}
+          {!nextTalk && (
+            <>
+              <FontAwesomeIcon icon={faCalendarAlt} color="white" size={10} />
+              <Text size="small">Click to see talk schedule</Text>
+            </>
+          )}
+        </Box>
+      </Tip>
     </Box>
   );
 }
 
 export default connect((
-  { habitat: { habitatInfo: { zoo } } },
+  { habitat: { habitatInfo: { zoo, _id: habitatId } } },
 ) => (
-  { weather: zoo?.weather, location: zoo?.location }
-))(WeatherWidget);
+  { weather: zoo?.weather, location: zoo?.location, habitatId }
+), {
+  showScheduleModalAction: showScheduleModal,
+})(WeatherWidget);
