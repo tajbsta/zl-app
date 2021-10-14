57cdcc491e261d69edd6651da7f9d762fd637e57
tajbsta <tajbsta22@gmail.com>
2021-10-14 10:59:02 -0400

    Adjust static viewer number to be random (#701)

--- src/components/ViewersCount/index.js
+++ src/components/ViewersCount/index.js
@@ -2,10 +2,8 @@ import { connect } from 'react-redux';
 
 import ViewsCount from './standalone';
 
-const VIEWER_ADJUSTMENT = 5;
-
 const ConnectedViewersCount = ({ plain, viewers, className }) => (
-  <ViewsCount viewers={ viewers + VIEWER_ADJUSTMENT } className={className} plain={plain} />
+  <ViewsCount viewers={viewers} className={className} plain={plain} />
 );
 
 export default connect(({ mainStream: { viewers }}) => ({ viewers }))(ConnectedViewersCount);
