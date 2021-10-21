51bcecbf16b69921155c62fc6ca705be3568b231
tajbsta <tajbsta22@gmail.com>
2021-10-21 12:08:53 -0400

    [179952042] - Add upgrade link to freemium habitat page and update styling (#720)

--- src/routes/freemiumOnboarding/index.js
+++ src/routes/freemiumOnboarding/index.js
@@ -1,5 +1,5 @@
-import { route } from 'preact-router';
-import { useMemo } from 'preact/hooks';
+import { route, Link } from 'preact-router';
+import { useMemo, useEffect } from 'preact/hooks';
 import { connect } from 'react-redux';
 import {
   Box,
@@ -15,6 +15,7 @@ import { buildURL, post } from 'Shared/fetch';
 import { logPageViewGA } from 'Shared/ga';
 
 import { setUserData } from '../../redux/actions';
+import { useIsMobileSize } from '../../hooks';
 
 import style from './style.module.scss';
 
@@ -24,13 +25,16 @@ const FreemiumOnboarding = ({
   isFreemiumOnboarded,
   setUserDataAction,
 }) => {
+  const isMobileSize = useIsMobileSize();
   const selectedHabitat = useMemo(() => allHabitats.find(
     ({ _id }) => _id === freeHabitat,
   ), [allHabitats, freeHabitat]);
   const paidHabitats = allHabitats.filter(({ _id }) => _id !== freeHabitat).slice(0, 10);
 
-  const goToHabitat = async () => {
-    if (!isFreemiumOnboarded) {
+  const goToHabitat = async () => route(`/h/${selectedHabitat?.zoo?.slug}/${selectedHabitat?.slug}`, true);
+
+  useEffect(() => {
+    const markFreemiumOnboarded = async () => {
       try {
         const { user } = await post(buildURL('/users/freemiumOnboarding'));
         logPageViewGA('/trialStarted');
@@ -44,8 +48,10 @@ const FreemiumOnboarding = ({
       }
     }
 
-    route(`/h/${selectedHabitat?.zoo?.slug}/${selectedHabitat?.slug}`, true);
-  }
+    if (!isFreemiumOnboarded) {
+      markFreemiumOnboarded();
+    }
+  }, [isFreemiumOnboarded, setUserDataAction])
 
   if (!selectedHabitat) {
     return null;
@@ -66,7 +72,7 @@ const FreemiumOnboarding = ({
         </Box>
         <Heading level="4" color="var(--charcoal)">Your free habitat:</Heading>
         <Heading level="2" color="var(--logoBlue)" textAlign="center">{selectedHabitat.title}</Heading>
-        <Box pad={{ horizontal: '77px' }} margin={{ top: '44px' }} justify="center" align="center">
+        <Box pad={{ horizontal: '77px' }} margin={{ top: isMobileSize ? '22px' : '44px' }} justify="center" align="center">
           <PrimaryButton label="Enter Habitat" onClick={goToHabitat} />
         </Box>
       </Box>
@@ -80,7 +86,8 @@ const FreemiumOnboarding = ({
         >
           <FontAwesomeIcon icon={faLockAlt} />
           <Text size="xlarge" color="var(--charcoal)">
-            Upgrade to unlock all animals.
+            <Link href="/plans">Upgrade </Link>
+            {` to unlock all animals.`}
           </Text>
         </Box>
         <Grid
