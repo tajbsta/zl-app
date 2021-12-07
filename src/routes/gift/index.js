6d921b65826dc95c589cb6a0fc4605fad06fbdb1
tajbsta <tajbsta22@gmail.com>
2021-12-07 09:57:13 -0500

    update gift page to have header and max width and gradient background (#820)

--- src/routes/gift/index.js
+++ src/routes/gift/index.js
@@ -1,11 +1,24 @@
-import Layout from '../../layouts/LoginSignup';
+import classnames from 'classnames';
+
+import Header from '../home/Header';
+import Carousel from '../../layouts/LoginSignup/Carousel';
 
 import style from './style.scss';
 
 const Gift = () => (
-  <Layout className={style.giftWrapper}>
-    <div className="gift-up-target" data-site-id="532caea9-ee00-447d-afab-114c9c57c7a9" data-platform="Other" />
-  </Layout>
+  <div className={style.giftPage}>
+    <Header />
+    <div className={style.contentContainer}>
+      <div className={style.contentWrapper}>
+        <Carousel />
+        <div className={classnames(style.formSection, 'customScrollBar grey')}>
+          <div className={style.formWrapper}>
+            <div className="gift-up-target" data-site-id="532caea9-ee00-447d-afab-114c9c57c7a9" data-platform="Other" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default Gift;
